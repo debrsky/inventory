2f71a71432fb721ae4883581e1c6af01c3d3b4cd
debrsky <debrsky@gmail.com>
2024-07-08 19:35:20 +1000

    refactoring: all db operations transfered to new module

---
+++ server/lib/db/index.js
@@ -0,0 +1,119 @@
+const config = require('../../../config.js');
+const fs = require('fs');
+const path = require('path');
+const os = require('os');
+const crypto = require('crypto');
+
+const resize = require('../utils/ffmpeg.js');
+
+const DB_DIR = config.dbDir;
+const CACHE_DIR = path.join(DB_DIR, 'CACHE');
+const ITEMS_DIR = path.join(DB_DIR, 'ITEMS');
+const INFO_FILE = 'info.json';
+
+const ID_SCHEMA = /^\d\d\d\d$/;
+
+async function getItems() {
+	const ids = (await fs.promises.readdir(ITEMS_DIR, { withFileTypes: true }))
+		.filter(file => file.isDirectory() && ID_SCHEMA.test(file.name))
+		.map(el => el.name);
+
+	const items = (await Promise.allSettled(
+		ids.map(id => fs.promises.readFile(path.join(ITEMS_DIR, id, INFO_FILE), 'utf8')))
+	).map((res, idx) => {
+		if (res.status === "rejected" && res.reason.code !== "ENOENT") throw Error(res.reason);
+		if (res.status === "rejected") return { id: ids[idx] };
+		// res.status === "fulfilled"
+		return { id: ids[idx], ...JSON.parse(res.value) };
+	});
+
+	return items;
+}
+
+async function getItem(id) {
+	const files = (await fs.promises.readdir(path.join(ITEMS_DIR, id), { withFileTypes: true }))
+		.filter(file => !file.isDirectory())
+		.map(el => el.name);
+
+	const infoPath = path.join(ITEMS_DIR, id, INFO_FILE);
+	let info;
+	try {
+		info = JSON.parse(await fs.promises.readFile(infoPath, 'utf8'));
+	} catch (err) {
+		if (err.code !== 'ENOENT') throw Error(err);
+	}
+
+	return { id, info, files };
+}
+
+async function getPathToFile({ id, file }) {
+	const pathToFile = path.resolve(path.join(ITEMS_DIR, id, file)).replace(/\\/g, '/');
+	await fs.promises.access(pathToFile, fs.constants.F_OK);
+	return pathToFile;
+}
+
+async function getPathToPreviewFile({ id, file }) {
+	const dir = path.resolve(path.join(CACHE_DIR, id));
+	const pathToFile = path.resolve(path.join(dir, file));
+
+	try {
+		await fs.promises.access(pathToFile, fs.constants.F_OK);
+	}
+	catch (err) {
+		if (err.code !== 'ENOENT') throw Error(err);
+
+		const pathToOriginalFile = path.resolve(path.join(ITEMS_DIR, id, file));
+		await fs.promises.access(pathToOriginalFile, fs.constants.F_OK);
+		await fs.promises.mkdir(dir, { recursive: true });
+		await resize(pathToOriginalFile, pathToFile);
+	}
+
+	return pathToFile;
+}
+
+async function addIds(count) {
+	const ids = (await fs.promises.readdir(ITEMS_DIR, { withFileTypes: true }))
+		.filter(file => file.isDirectory() && ID_SCHEMA.test(file.name))
+		.map(el => Number(el.name))
+		.sort((a, b) => a - b);
+	const lastId = ids.at(-1);
+	const newIds = Array.from({ length: count }, (_, i) => (lastId + 1 + i).toString());
+
+	await Promise.all(newIds.map(newId => fs.promises.mkdir(path.join(ITEMS_DIR, newId))));
+}
+
+async function removeFile({ id, file }) {
+	const pathToFile = path.join(ITEMS_DIR, id, file);
+	await fs.promises.unlink(pathToFile);
+	// TODO remove cached preview files
+}
+
+async function createWriteFileStream({ id, file }) {
+	const tempdir = os.tmpdir();
+	const tempfname = crypto.randomUUID();
+	const pathToFile = path.join(ITEMS_DIR, id, file);
+	const pathToTempFile = path.join(tempdir, tempfname);
+	const ws = fs.createWriteStream(pathToTempFile)
+		.on('finish', () => {
+			fs.rename(pathToTempFile, pathToFile, (err) => { if (err) console.error(err) });
+		});
+
+	return ws;
+}
+
+async function writeInfo({ id, info }) {
+	const infoPath = path.join(ITEMS_DIR, id, INFO_FILE);
+	// TODO validate info
+	await fs.promises.writeFile(infoPath, JSON.stringify(info, null, 4));
+}
+module.exports = {
+	getItems,
+	getItem,
+	getPathToFile,
+	getPathToPreviewFile,
+	addIds,
+	removeFile,
+	createWriteFileStream,
+	writeInfo,
+	ID_SCHEMA
+}
--- server/routes/inventory/index.js
+++ server/routes/inventory/index.js
@@ -9,36 +9,17 @@ send.mime.define({
 	'image/avif': ['avif']
 });
 
-const DB_DIR = config.dbDir;
-const CACHE_DIR = path.join(DB_DIR, 'CACHE');
-const ITEMS_DIR = path.join(DB_DIR, 'ITEMS');
-const INFO_FILE = 'info.json';
-const REPORT_FILE = 'Report.xml';
-
-const ID_SCHEMA = /^\d\d\d\d$/;
-
 const iconv = require('iconv-lite');
 const busboy = require('busboy');
 
 const express = require('express');
 const router = express.Router();
 
-const resize = require('../../lib/utils/ffmpeg.js');
+const DB = require('../../lib/db/index.js');
 
 router.get('/', async function (req, res, next) {
 	try {
-		const ids = (await fs.promises.readdir(ITEMS_DIR, { withFileTypes: true }))
-			.filter(file => file.isDirectory() && ID_SCHEMA.test(file.name))
-			.map(el => el.name);
-
-		const items = (await Promise.allSettled(
-			ids.map(id => fs.promises.readFile(path.join(ITEMS_DIR, id, INFO_FILE), 'utf8')))
-		).map((res, idx) => {
-			if (res.status === "rejected" && res.reason.code !== "ENOENT") throw Error(res.reason.code);
-			if (res.status === "rejected") return { id: ids[idx] };
-			// res.status === "fulfilled"
-			return { id: ids[idx], ...JSON.parse(res.value) };
-		});
+		const items = await DB.getItems();
 
 		res.render('inventory/index', { title: 'Инвентаризация', baseUrl: req.baseUrl, items });
 
@@ -49,12 +30,7 @@ router.get('/', async function (req, res, next) {
 
 router.post('/', async function (req, res, next) {
 	try {
-		const ids = (await fs.promises.readdir(ITEMS_DIR, { withFileTypes: true }))
-			.filter(file => file.isDirectory() && ID_SCHEMA.test(file.name))
-			.map(el => Number(el.name))
-			.sort((a, b) => a - b);
-		const newId = ((ids.at(-1)) + 1).toString();
-		await fs.promises.mkdir(path.join(ITEMS_DIR, newId));
+		await DB.addIds(5);
 		res.redirect(req.originalUrl);
 	} catch (err) {
 		return next(err);
@@ -63,29 +39,13 @@ router.post('/', async function (req, res, next) {
 
 router.get(['/items/:id', '/items/:id/edit'], async function (req, res, next) {
 	const id = req.params.id;
-	if (!ID_SCHEMA.test(id)) return next();
+	if (!DB.ID_SCHEMA.test(id)) return next();
 
 	const mode = req.url.endsWith('/edit') ? 'edit' : 'view';
 
-	const item = {
-		id, type: "", brand: "", model: "", place: "", comment: ""
-	};
-
 	try {
-		const files = (await fs.promises.readdir(path.join(ITEMS_DIR, id), { withFileTypes: true }))
-			.filter(file => !file.isDirectory())
-			.map(el => el.name);
-
-		const infoPath = path.join(ITEMS_DIR, id, INFO_FILE);
-		try {
-			const info = JSON.parse(await fs.promises.readFile(infoPath, 'utf8'));
-			Object.assign(item, info);
-		} catch (err) {
-			if (err.code !== 'ENOENT') throw Error(err);
-		}
-
-		res.render('inventory/item', { title: `${item.id} Инвентаризация`, baseUrl: req.baseUrl, mode, item, files, content: files.join("\n") });
-
+		const item = await DB.getItem(id);
+		res.render('inventory/item', { title: `${item.id} Инвентаризация`, baseUrl: req.baseUrl, mode, item });
 	} catch (err) {
 		if (err.code === 'ENOENT') return next();
 		return next(err);
@@ -94,24 +54,18 @@ router.get(['/items/:id', '/items/:id/edit'], async function (req, res, next) {
 
 router.post('/items/:id', async function (req, res, next) {
 	const id = req.params.id;
-	if (!ID_SCHEMA.test(id)) return next();
+	if (!DB.ID_SCHEMA.test(id)) return next();
 
 	const data = {};
 	const filesToRemove = [];
 
 
 	const bb = busboy({ headers: req.headers });
-	bb.on('file', (name, file, info) => {
+	bb.on('file', async (name, file, info) => {
 		const { filename, encoding, mimeType } = info;
 
 		if (name === "file" && filename) {
-			const tempdir = os.tmpdir();
-			const tempfname = crypto.randomUUID();
-			const pathToFile = path.join(ITEMS_DIR, id, filename);
-			const pathToTempFile = path.join(tempdir, tempfname);
-			const ws = fs.createWriteStream(pathToTempFile).on('finish', () => {
-				fs.rename(pathToTempFile, pathToFile, (err) => { if (err) console.error(err) });
-			});
+			const ws = await DB.createWriteFileStream({ id, file: filename });
 			file.pipe(ws);
 		} else {
 			file.on('data', () => { }); // no file
@@ -134,32 +88,24 @@ router.post('/items/:id', async function (req, res, next) {
 		data[name] = val;
 	});
 	bb.on('close', async () => {
-		if (filesToRemove.length > 0) {
-			try {
-				for (const filename of filesToRemove) {
-					const pathToFile = path.join(ITEMS_DIR, id, filename);
-					await fs.promises.unlink(pathToFile);
-				}
-			} catch (err) {
-				return next(err);
-			}
-		}
-
-		const infoPath = path.join(ITEMS_DIR, id, INFO_FILE);
-
 		try {
-			await fs.promises.writeFile(infoPath, JSON.stringify(data, null, 4));
+			if (filesToRemove.length > 0) {
+				await Promise.all(filesToRemove.map(file => DB.removeFile({ id, file })));
+			}
+
+			await DB.writeInfo({ id, info: data });
+
+			const searchStr = (req.originalUrl.match(/\?.*$/) ?? [])[0];
+			const searchParams = new URLSearchParams(searchStr);
+			searchParams.delete('mode');
+			const newSearchStr = searchParams.size > 0 ? `?${searchParams.toString()}` : '';
+			const url = req.originalUrl.replace(searchStr, newSearchStr);
+
+			res.redirect(url);
+
 		} catch (err) {
 			return next(err);
 		}
-
-		const searchStr = (req.originalUrl.match(/\?.*$/) ?? [])[0];
-		const searchParams = new URLSearchParams(searchStr);
-		searchParams.delete('mode');
-		const newSearchStr = searchParams.size > 0 ? `?${searchParams.toString()}` : '';
-		const url = req.originalUrl.replace(searchStr, newSearchStr);
-
-		res.redirect(url);
 	});
 	bb.on('error', (err) => {
 		next(err);
@@ -169,74 +115,38 @@ router.post('/items/:id', async function (req, res, next) {
 });
 
 router.get('/items/:id/:file', async function (req, res, next) {
-	const id = req.params.id;
-	if (!ID_SCHEMA.test(id)) return next();
-
-	const file = req.params.file;
-
-	const pathToFile = path.resolve(path.join(ITEMS_DIR, id, file)).replace(/\\/g, '/');
-
-	let fileStat;
 	try {
-		fileStat = await fs.promises.stat(pathToFile);
-	}
-	catch (err) {
+		const id = req.params.id;
+		if (!DB.ID_SCHEMA.test(id)) return next();
+
+		const file = req.params.file;
+
+		const pathToFile = await DB.getPathToFile({ id, file });
+
+		const stream = send(req, pathToFile)
+			.on('error', (err) => {
+				if (err.code === 'ENOENT') return next();
+				return next(err);
+			}).on('headers', (res, path, stat) => {
+				if (/Report.htm$/.test(path)) {
+					res.setHeader('Content-Type', 'text/html; charset=Windows-1251')
+				};
+			});
+
+		stream.pipe(res);
+	} catch (err) {
 		if (err.code === 'ENOENT') return next();
 		return next(err);
 	}
-
-	// const ext = path.extname(file).slice(1).toLowerCase();
-
-	// const stream = fs.createReadStream(pathToFile)
-	// 	.on('error', (err) => {
-	// 		if (err.code === 'ENOENT') return next();
-	// 		return next(err);
-	// 	});
-	const stream = send(req, pathToFile)
-		.on('error', (err) => {
-			if (err.code === 'ENOENT') return next();
-			return next(err);
-		}).on('headers', (res, path, stat) => {
-			if (/Report.htm$/.test(path)) {
-				res.setHeader('Content-Type', 'text/html; charset=Windows-1251')
-			};
-		});
-
-	stream.pipe(res);
-
-	// res.on('close', () => { stream.destroy(); converterStream?.destroy() });
 });
 
 router.get('/items/:id/preview/:file', async function (req, res, next) {
 	const id = req.params.id;
-	if (!ID_SCHEMA.test(id)) return next();
+	if (!DB.ID_SCHEMA.test(id)) return next();
 
 	const file = req.params.file;
 
-	const dir = path.resolve(path.join(CACHE_DIR, id));
-	const pathToFile = path.resolve(path.join(dir, file));
-
-	try {
-		await fs.promises.stat(pathToFile);
-	}
-	catch (err) {
-		if (err.code !== 'ENOENT') return next(err);
-
-		const pathToOriginalFile = path.resolve(path.join(ITEMS_DIR, id, file));
-		try {
-			await fs.promises.stat(pathToOriginalFile);
-		} catch (err) {
-			if (err.code !== 'ENOENT') return next(err);
-			return next();
-		}
-
-		await fs.promises.mkdir(dir, { recursive: true })
-		try {
-			await resize(pathToOriginalFile, pathToFile);
-		} catch (err) {
-			return next(err);
-		}
-	}
+	const pathToFile = await DB.getPathToPreviewFile({ id, file });
 
 	const stream = send(req, pathToFile)
 		.on('error', (err) => {
@@ -249,8 +159,6 @@ router.get('/items/:id/preview/:file', async function (req, res, next) {
 		});
 
 	stream.pipe(res);
-
-	// res.on('close', () => { stream.destroy() });
 });
 
 module.exports = router;
