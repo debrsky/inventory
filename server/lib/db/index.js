c4fbd7b2962cdaee40a3f4368efc9c94f771391b
debrsky <debrsky@gmail.com>
2024-07-10 11:54:07 +1000

    add detailed JSDoc comments for functions in db/index.js

--- server/lib/db/index.js
+++ server/lib/db/index.js
@@ -12,8 +12,20 @@ const CACHE_DIR = path.join(DB_DIR, 'CACHE');
 const ITEMS_DIR = path.join(DB_DIR, 'ITEMS');
 const INFO_FILE = 'info.json';
 
+/**
+ * Regular expression to validate item IDs.
+ * IDs should be exactly four digits long.
+ * @type {RegExp}
+ */
 const ID_SCHEMA = /^\d\d\d\d$/;
 
+/**
+ * Retrieves a list of items from the ITEMS_DIR directory.
+ * Each item is represented by its ID and corresponding info from the INFO_FILE.
+ * @async
+ * @function getItems
+ * @returns {Promise<Array<Object>>} An array of objects, each containing an item's ID and info.
+ */
 async function getItems() {
 	await fs.promises.mkdir(ITEMS_DIR, { recursive: true });
 
@@ -33,6 +45,13 @@ async function getItems() {
 	return items;
 }
 
+/**
+ * Retrieves detailed information about a specific item by its ID.
+ * @async
+ * @function getItem
+ * @param {string} id - The ID of the item to retrieve.
+ * @returns {Promise<Object>} An object containing the item's ID, detailed info, and list of files.
+ */
 async function getItem(id) {
 	const files = (await fs.promises.readdir(path.join(ITEMS_DIR, id), { withFileTypes: true }))
 		.filter(file => !file.isDirectory())
@@ -50,20 +69,38 @@ async function getItem(id) {
 	return { id, info, files };
 }
 
+/**
+ * Retrieves the path to a specific file within an item's directory.
+ * @async
+ * @function getPathToFile
+ * @param {string} id - The ID of the item.
+ * @param {string} file - The name of the file to retrieve.
+ * @returns {Promise<string>} The resolved path to the file.
+ * @throws {Error} If the file does not exist.
+ */
 async function getPathToFile(id, file) {
 	const pathToFile = path.resolve(path.join(ITEMS_DIR, id, file)).replace(/\\/g, '/');
 	await fs.promises.access(pathToFile, fs.constants.F_OK);
 	return pathToFile;
 }
 
+/**
+ * Retrieves the path to a preview file for a specific item.
+ * If the preview file does not exist, it attempts to create it from the original file.
+ * @async
+ * @function getPathToPreviewFile
+ * @param {string} id - The ID of the item.
+ * @param {string} file - The name of the file to retrieve.
+ * @returns {Promise<string>} The resolved path to the preview file.
+ * @throws {Error} If the file does not exist or if there is an error during resizing.
+ */
 async function getPathToPreviewFile(id, file) {
 	const dir = path.resolve(path.join(CACHE_DIR, id));
 	const pathToFile = path.resolve(path.join(dir, file));
 
 	try {
 		await fs.promises.access(pathToFile, fs.constants.F_OK);
-	}
-	catch (err) {
+	} catch (err) {
 		if (err.code !== 'ENOENT') throw Error(err);
 
 		const pathToOriginalFile = path.resolve(path.join(ITEMS_DIR, id, file));
@@ -79,6 +116,13 @@ async function getPathToPreviewFile(id, file) {
 	return pathToFile;
 }
 
+/**
+ * Adds a specified number of new IDs to the ITEMS_DIR directory.
+ * @async
+ * @function addIds
+ * @param {number} count - The number of new IDs to add.
+ * @returns {Promise<void>} A promise that resolves when all new IDs have been added.
+ */
 async function addIds(count) {
 	const ids = (await fs.promises.readdir(ITEMS_DIR, { withFileTypes: true }))
 		.filter(file => file.isDirectory() && ID_SCHEMA.test(file.name))
@@ -91,12 +135,35 @@ async function addIds(count) {
 	await Promise.all(newIds.map(newId => fs.promises.mkdir(path.join(ITEMS_DIR, newId), { recursive: true })));
 }
 
+/**
+ * Removes a specified file from an item's directory.
+ * @async
+ * @function removeFile
+ * @param {string} id - The ID of the item.
+ * @param {string} file - The name of the file to remove.
+ * @returns {Promise<void>} A promise that resolves when the file has been removed.
+ * @throws {Error} If there is an error during the removal process.
+ */
 async function removeFile(id, file) {
 	const pathToFile = path.join(ITEMS_DIR, id, file);
-	await fs.promises.unlink(pathToFile);
-	// TODO remove cached preview files
+	try {
+		await fs.promises.unlink(pathToFile);
+		// TODO: Implement the removal of cached preview files
+	} catch (err) {
+		if (err.code !== 'ENOENT') throw Error(err);
+	}
 }
 
+/**
+ * Creates a writable stream for writing a file to a specified item's directory.
+ * The file is first written to a temporary location and then moved to the final destination upon completion.
+ * @async
+ * @function createWriteFileStream
+ * @param {string} id - The ID of the item.
+ * @param {string} file - The name of the file to write.
+ * @returns {Promise<fs.WriteStream>} A writable stream for the file.
+ * @throws {Error} If there is an error during the file writing process.
+ */
 async function createWriteFileStream(id, file) {
 	const tempdir = os.tmpdir();
 	const tempfname = crypto.randomUUID();
@@ -116,7 +183,16 @@ async function createWriteFileStream(id, file) {
 
 	return ws;
 }
-
+/**
+ * Writes information to a specified item's directory.
+ * The information is written to a file named INFO_FILE in JSON format.
+ * @async
+ * @function writeInfo
+ * @param {string} id - The ID of the item.
+ * @param {Object} info - The information to write.
+ * @returns {Promise<void>} A promise that resolves when the information has been written.
+ * @throws {Error} If there is an error during the writing process.
+ */
 async function writeInfo(id, info) {
 	const dir = path.join(ITEMS_DIR, id);
 	await fs.promises.mkdir(dir, { recursive: true });
@@ -124,6 +200,7 @@ async function writeInfo(id, info) {
 	// TODO validate info
 	await writeFileAtomic(pathToFile, JSON.stringify(info, null, 4));
 }
+
 module.exports = {
 	getItems,
 	getItem,
