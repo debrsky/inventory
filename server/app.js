943b2dddafb75120ec3add9e4df0e2ee05ddf0d8
debrsky <debrsky@gmail.com>
2024-07-11 19:49:54 +1000

    remove development environment check

--- server/app.js
+++ server/app.js
@@ -43,7 +43,7 @@ setAuthorize(app);
 
 app.use(express.static(config.public));
 
-if (app.get('env') !== 'development') app.use(isLoggedIn);
+app.use(isLoggedIn);
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
