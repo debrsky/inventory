4c503d523fcca9fa52ff6f66ee73de8d6ae7ac5e
debrsky <debrsky@gmail.com>
2024-07-13 15:34:07 +1000

    add SESSION_SECRET from environment variable

--- server/app.js
+++ server/app.js
@@ -49,7 +49,6 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/inventory', inventoryRouter);
 
-
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
 	next(createError(404));
--- server/lib/sessions.js
+++ server/lib/sessions.js
@@ -1,4 +1,5 @@
 const config = require('../../config.js');
+const SESSION_SECRET = process.env.SESSION_SECRET;
 
 const fs = require('fs');
 const path = require('path');
@@ -7,7 +8,7 @@ const FileStore = require('session-file-store')(session);
 
 function sessions() {
 	const sessionStorage = path.join(config.rootDir, config.storage, 'sessions');
-	fs.mkdirSync(sessionStorage, {recursive: true});
+	fs.mkdirSync(sessionStorage, { recursive: true });
 	const fileStoreOptions = {
 		path: sessionStorage,
 		ttl: 60 * 60 * 24
@@ -15,10 +16,10 @@ function sessions() {
 
 	const sessionOptions = {
 		store: new FileStore(fileStoreOptions),
-		secret: 'keyboard cat top secret',
+		secret: SESSION_SECRET,
 		resave: true,
 		saveUninitialized: true,
-		cookie: {secure: 'auto'}
+		cookie: { secure: 'auto' }
 	};
 	return session(sessionOptions);
 }
